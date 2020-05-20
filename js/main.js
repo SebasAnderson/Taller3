ccc237eb1334f58bafc716fbf009e6d27c2c7e2b
Sebas <sebastian96anderson@gmail,com>
2020-05-20 19:31:15 -0300

    Mapa

--- js/main.js
+++ js/main.js
@@ -3,6 +3,18 @@
 
   var regalo = document.getElementById("regalo");
   document.addEventListener("DOMContentLoaded", function () {
+
+    var map = L.map("mapa").setView([51.505, -0.09], 13);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker([51.5, -0.09])
+      .addTo(map)
+      .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+      .openPopup();
+
     //Campos Datos Usuario
     var nombre = document.getElementById("nombre");
     var apellido = document.getElementById("apellido");
@@ -22,9 +34,38 @@
     var stickers = document.getElementById("stickers");
 
     calcular.addEventListener("click", calcularMontos);
+
     pase_dia.addEventListener("blur", mostrarDias);
     pase_dosDias.addEventListener("blur", mostrarDias);
     pase_completo.addEventListener("blur", mostrarDias);
+
+    nombre.addEventListener("blur", validarCampos);
+    apellido.addEventListener("blur", validarCampos);
+    email.addEventListener("blur", validarCampos);
+    email.addEventListener("blur", validarMail);
+
+    function validarCampos() {
+      if (this.value == "") {
+        errorDiv.style.display = "block";
+        errorDiv.innerHTML = "Este campo es obligatorio";
+        this.style.border = "1px solid red";
+        errorDiv.style.border = "1px solid red";
+      } else {
+        errorDiv.style.display = "none";
+        this.style.border = "1px solid #cccccc";
+      }
+    }
+    function validarMail() {
+      if (this.value.indexOf("@") > -1) {
+        errorDiv.style.display = "none";
+        this.style.border = "1px solid #cccccc";
+      } else {
+        errorDiv.style.display = "block";
+        errorDiv.innerHTML = "Debe tener al menos una @";
+        this.style.border = "1px solid red";
+        errorDiv.style.border = "1px solid red";
+      }
+    }
 
     function calcularMontos(event) {
       event.preventDefault();
@@ -70,30 +111,30 @@
           lista_productos.innerHTML += listadoProductos[1] + "<br/>";
         }
         suma.innerHTML = "$ " + totalPagar.toFixed(2);
-      }}
-      function mostrarDias() {
-        var boletosDia = parseInt(pase_dia.value, 10) || 0,
-          boletos2Dias = parseInt(pase_dosDias.value, 10) || 0,
-          boletosCompleto = parseInt(pase_completo.value, 10) || 0;
-
-        var diasElegidos = [];
-        console.log(boletosDia);
-        if (boletosDia > 0) {
-          diasElegidos.push('viernes');
-          console.log(diasElegidos);
-        }
-        if (boletos2Dias > 0) {
-          diasElegidos.push('viernes', 'sabado');
-          console.log(diasElegidos);
-        }
-        if (boletosCompleto > 0) {
-          diasElegidos.push("viernes", "sabado", "domingo");
-          console.log(diasElegidos);
-        }
-        for (var i = 0; i < diasElegidos.length; i++) {
-          document.getElementById(diasElegidos[i]).style.display = 'block';
-        }
       }
     }
-  ); //Dom content loaded
+    function mostrarDias() {
+      var boletosDia = parseInt(pase_dia.value, 10) || 0,
+        boletos2Dias = parseInt(pase_dosDias.value, 10) || 0,
+        boletosCompleto = parseInt(pase_completo.value, 10) || 0;
+
+      var diasElegidos = [];
+      // console.log(boletosDia);
+      if (boletosDia > 0) {
+        diasElegidos.push("viernes");
+        console.log(diasElegidos);
+      }
+      if (boletos2Dias > 0) {
+        diasElegidos.push("viernes", "sabado");
+        console.log(diasElegidos);
+      }
+      if (boletosCompleto > 0) {
+        diasElegidos.push("viernes", "sabado", "domingo");
+        console.log(diasElegidos);
+      }
+      for (var i = 0; i < diasElegidos.length; i++) {
+        document.getElementById(diasElegidos[i]).style.display = "block";
+      }
+    }
+  }); //Dom content loaded
 })();
