8976c103da588459d6e6d28de1951f4ce2ccd450
Sebas <sebastian96anderson@gmail,com>
2020-06-26 16:33:17 -0300

    ajustes de elementos

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -5,7 +5,6 @@
     <title></title>
     <meta name="description" content="" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-
     <link rel="manifest" href="site.webmanifest" />
     <link rel="apple-touch-icon" href="icon.png" />
     <link rel="stylesheet" href="./css/normalize.css" />
